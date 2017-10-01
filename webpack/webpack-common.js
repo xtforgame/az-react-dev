5b82e47253132d480785c65e07d1bfaa588b1569
escargot <xtforgame@gmail.com>
2017-10-01 18:25:01 +0800

    Add the transform-object-rest-spread package

--- webpack/webpack-common.js
+++ webpack/webpack-common.js
@@ -63,6 +63,7 @@ module.exports = function(env) {
             plugins: [
               'transform-decorators-legacy',
               'transform-class-properties',
+              'transform-object-rest-spread',
             ],
           },
           exclude: /node_modules/,
