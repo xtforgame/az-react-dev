3d907167d3e17d75ca794c3bb6ee725e25d9248a
RickChen <xtforgame@gmail.com>
2017-12-22 07:43:24 +0800

    Fixed a bug about unexpected unmount and mount behaviors of EnhancedRoute

--- src/front-end/components/routes/EnhancedRoute/index.js
+++ src/front-end/components/routes/EnhancedRoute/index.js
@@ -2,16 +2,31 @@ import React from 'react';
 import { Route } from 'react-router';
 
 class EnhancedRoute extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {component: null};
+  }
+
+  componentWillMount(){
+    // [TODO] Maybe the route should be rename to something like `FixedRoute`
+    //        to remind the user that the route dosen't support component changing
+    // It makes `component` unable to get updated from props
+    // for avoiding unexpected unmount and mount
+    this.setState({component: this.props.component});
+  }
+
   render(){
     let {
       routeName,
-      component: Component,
+      component,
       routeView,
       routeViews,
       ...rest,
     } = this.props;
 
     // console.log(`EnhancedRoute render: ${routeName}`);
+
+    let Component = this.state.component;
 
     return (
       <Route
